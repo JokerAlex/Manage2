21232296f3c1b69f8c5754f4f64d871a55cfc3ee
JokerAlex <alex.zhao1023@gmail.com>
2019-05-20 21:42:45 +0800

    update result

--- src/main/java/com/dzhy/manage/common/Result.java
+++ src/main/java/com/dzhy/manage/common/Result.java
@@ -16,65 +16,66 @@ import lombok.ToString;
 public class Result<T> {
     private int status;
     private String msg;
-
     private T data;
+    private long timestamp;
 
-    private Result(int status){
-        this.status = status;
-    }
-    private Result(int status, T data){
-        this.status = status;
-        this.data = data;
+    private Result(int status) {
+        this(status, null, null);
     }
 
-    private Result(int status, String msg, T data){
+    private Result(int status, T data) {
+        this(status, null, data);
+    }
+
+    private Result(int status, String msg) {
+        this(status, msg, null);
+    }
+
+    public Result(int status, String msg, T data) {
         this.status = status;
         this.msg = msg;
         this.data = data;
-    }
-
-    private Result(int status, String msg){
-        this.status = status;
-        this.msg = msg;
+        this.timestamp = System.currentTimeMillis();
     }
 
     /**
      * 判断请求是否成功
      * 注解使之不在json序列化结果当中
+     *
      * @return
      */
     @JsonIgnore
-    public boolean isOk(){
+    public boolean isOk() {
         return this.status == ResultEnum.SUCCESS.getCode();
     }
 
-    public static <T> Result<T> isSuccess(){
+    public static <T> Result<T> isSuccess() {
         return new Result<T>(ResultEnum.SUCCESS.getCode());
     }
 
-    public static <T> Result<T> isSuccess(String msg){
-        return new Result<T>(ResultEnum.SUCCESS.getCode(),msg);
+    public static <T> Result<T> isSuccess(String msg) {
+        return new Result<T>(ResultEnum.SUCCESS.getCode(), msg);
     }
 
-    public static <T> Result<T> isSuccess(T data){
-        return new Result<T>(ResultEnum.SUCCESS.getCode(),data);
+    public static <T> Result<T> isSuccess(T data) {
+        return new Result<T>(ResultEnum.SUCCESS.getCode(), data);
     }
 
-    public static <T> Result<T> isSuccess(String msg, T data){
-        return new Result<T>(ResultEnum.SUCCESS.getCode(),msg,data);
-    }
-
-
-    public static <T> Result<T> isError(){
-        return new Result<T>(ResultEnum.ERROR.getCode(),ResultEnum.ERROR.getMessage());
+    public static <T> Result<T> isSuccess(String msg, T data) {
+        return new Result<T>(ResultEnum.SUCCESS.getCode(), msg, data);
     }
 
 
-    public static <T> Result<T> isError(String errorMessage){
-        return new Result<T>(ResultEnum.ERROR.getCode(),errorMessage);
+    public static <T> Result<T> isError() {
+        return new Result<T>(ResultEnum.ERROR.getCode(), ResultEnum.ERROR.getMessage());
     }
 
-    public static <T> Result<T> isError(int errorCode, String errorMessage){
-        return new Result<T>(errorCode,errorMessage);
+
+    public static <T> Result<T> isError(String errorMessage) {
+        return new Result<T>(ResultEnum.ERROR.getCode(), errorMessage);
+    }
+
+    public static <T> Result<T> isError(int errorCode, String errorMessage) {
+        return new Result<T>(errorCode, errorMessage);
     }
 }
